d2e72fb4608f3d3310c71a95e2e0b149fb4046f5
ivayloynikolov <ifcho@abv.bg>
2024-11-18 18:37:15 +0200

    network controller implementation

--- Menu Adviser/Controller/NetworkController.swift
+++ Menu Adviser/Controller/NetworkController.swift
@@ -7,8 +7,17 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case invalidURL
+    case invalidResponse
+    case noData
+    case decodeError(message: String, error: Error?)
+}
+
 class NetworkController {
     static let shared = NetworkController()
+    
+    let SERVER_URL: String = "https://a-pps.net/"
     
     private init() {}
     
@@ -18,5 +27,60 @@ class NetworkController {
         JSONDataController.shared.getMockRecipe(recipeRequestData: recipeRequestData, completion: { response in
             completion(response)
         })
+    }
+    
+    func getRecipeDataFromServer(recipeRequestData: RecipeRequestData, completion: (RecipeResponseData?) -> Void) {
+        
+        let queryItems = [
+            URLQueryItem(name: "recipeTypes", value: recipeRequestData.recipeTypes),
+            URLQueryItem(name: "caloriesFrom", value: String(recipeRequestData.caloriesFrom ?? 0)),
+            URLQueryItem(name: "caloriesTo", value: String(recipeRequestData.caloriesTo ?? 2000)),
+            URLQueryItem(name: "carbPercentageFrom", value: String(recipeRequestData.carbPercentageFrom ?? 0)),
+            URLQueryItem(name: "carbPercentageTo", value: String(recipeRequestData.carbPercentageTo ?? 100)),
+            URLQueryItem(name: "carbPercentageFrom", value: String(recipeRequestData.carbPercentageFrom ?? 0)),
+            URLQueryItem(name: "carbPercentageTo", value: String(recipeRequestData.carbPercentageTo ?? 100)),
+            URLQueryItem(name: "carbPercentageFrom", value: String(recipeRequestData.carbPercentageFrom ?? 0)),
+            URLQueryItem(name: "carbPercentageTo", value: String(recipeRequestData.carbPercentageTo ?? 100))
+        ]
+        
+        guard let url = URL(string: SERVER_URL)?
+            .appending(queryItems: queryItems)
+        else {
+            print("Invalid URL")
+            return
+        }
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+                print("Invalid Response")
+                return
+            }
+            
+            guard let data else {
+                print("No data received")
+                return
+            }
+            
+            do {
+                let recipeData = try JSONDecoder().decode(RecipeResponseData.self, from: data)
+            
+            } catch {
+              print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    enum CodingKeys: String, CodingKey {
+      case id
+      case categoryDescription = "description"
+      case iconString = "icon"
     }
 }
